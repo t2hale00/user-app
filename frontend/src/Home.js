a6cc324d8e8fcdf760217febf26bc60372024068
Lea <112546991+t2hale00@users.noreply.github.com>
2023-11-09 13:15:12 +0200

    Edited home.js

--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -7,17 +7,9 @@ function Home() {
         <div className='bg-white p-3 rounded w-25'>
           <h2>Log In</h2>
           <form action="">
-            <div className='mb-3'>
-              <label htmlFor="email"><strong>Email</strong></label>
-              <input type="email" className='form-control rounded-0' placeholder=' Enter email' name='email' />
-             {errors.email &&  <span className='text-danger'>{errors.email}</span>}
-            </div>
+            
   
-            <div className='mb-3'>
-              <label htmlFor="password"><strong>Password</strong></label>
-              <input type="password" className='form-control rounded-0' placeholder=' Enter password' name='password'/>
-              {errors.password &&  <span className='text-danger'>{errors.password}</span>}
-            </div>
+            
   
             <button type='submit' className='btn btn-success w-100 rounded-0'>Send Parcel</button>
             
