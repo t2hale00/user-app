ad13fee1bf6b3239a874a756f68bfb9936fa5b73
Lea <112546991+t2hale00@users.noreply.github.com>
2023-11-23 02:38:35 +0200

    Edited file

--- frontend/src/DeleteAccount.js
+++ frontend/src/DeleteAccount.js
@@ -1,17 +1,3 @@
-/*import React from 'react'
-
-function AccountSettings() {
-  return (
-    <div className='d-flex justify-content-center align-items-center bg-primary vh-100'>  
-      <div className='bg-white p-3 rounded w-25 '>
-        <button type="submit" className='btn btn-danger w-100 rounded-0'>Delete Account</button>
-      </div>
-    </div>
-
-  )
-}
-
-export default AccountSettings;*/
 import React, { useState } from 'react';
 
 const DeleteAccountButton = () => {
