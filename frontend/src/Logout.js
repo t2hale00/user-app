1c9334cdaa9b6cba1e6c31db42f823f5212f4d78
Lea <112546991+t2hale00@users.noreply.github.com>
2023-11-30 13:44:43 +0200

    Added notification to logout file

--- frontend/src/Logout.js
+++ frontend/src/Logout.js
@@ -1,27 +1,35 @@
 import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
-import { Spinner } from 'react-bootstrap'; // Import Spinner from react-bootstrap
+import { Spinner, Alert } from 'react-bootstrap';
 
 function Logout() {
   const navigate = useNavigate();
   const [loading, setLoading] = useState(true);
+  const [notification, setNotification] = useState(null);
 
   useEffect(() => {
     const handleLogout = async () => {
       try {
+        console.log('Attempting logout...');
         // Make a request to the server to log out
         await axios.get('http://localhost:8081/logout');
-        
+
         // Simulate a delay for demonstration purposes (remove this line in production)
         await new Promise(resolve => setTimeout(resolve, 2000));
 
-        // Redirect to the login page or any other desired route
-        navigate('/');
+        // Set the notification message
+        setNotification('Logged out successfully. Thank you for using the app.');
+
+        console.log('Logout successful');
+        // Delay redirect to give time for the user to see the notification
+        setTimeout(() => {
+          console.log('Redirecting...');
+          navigate('/');
+        }, 3000);
       } catch (error) {
         console.error('Logout error:', error);
-      } finally {
-        setLoading(false); // Set loading to false when the process is complete
+        setLoading(false); // Set loading to false even if an error occurs
       }
     };
 
@@ -29,17 +37,23 @@ function Logout() {
     handleLogout();
   }, [navigate]);
 
-  return (
+  return (  
     <div className="text-center mt-5">
       {loading ? (
         <Spinner animation="border" role="status">
           <span className="visually-hidden">Loading...</span>
         </Spinner>
       ) : (
-        <p>Logging out...</p>
+        <>
+          <p>Logging out...</p>
+        </>
       )}
+      {notification && <Alert variant="success">{notification}</Alert>}
     </div>
   );
+
 }
 
+
 export default Logout;
+
